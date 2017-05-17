7fa5dbaf3f355c1314e09c0d1c4519b99ec95cd7
mmeta <mmeta@157.181.198.96>
2017-05-17 20:55:59 +0200

    Basic view.

---
+++ torpedo/Board.java
@@ -0,0 +1,110 @@
+
+package torpedo;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Board extends JPanel implements ActionListener, MouseListener, MouseMotionListener {
+
+    TorpedoView view;
+
+    public Board() {
+        initBoard();
+
+    }
+
+    private void initBoard() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenSize.getWidth();
+
+        //setPreferredSize(new Dimension((int) (screenSize.getWidth() - 300), (int) ((screenSize.getWidth() - 300) / 16 * 9)));
+        setPreferredSize(new Dimension(800, 500));
+        addKeyListener(new Controller());
+        this.addMouseMotionListener(this);
+        this.addMouseListener(this);
+        setFocusable(true);
+        setBackground(new Color(1, 95, 135));
+        setDoubleBuffered(true);
+        view = new TorpedoView(getPreferredSize().getWidth(),getPreferredSize().getHeight());
+        
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        view.draw(g);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    void update() {
+       
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        
+    }
+
+    private class Controller extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+           
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+           
+        }
+    }
+
+}
---
+++ torpedo/Torpedo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package torpedo;
+
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author mmeta
+ */
+public class Torpedo extends JFrame {
+
+    public Torpedo() {
+
+        initUI();
+
+    }
+
+    private void initUI() {
+        add((JPanel)new Board());
+        setTitle("Torpedo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+
+  
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            Torpedo torpedo = new Torpedo();
+            torpedo.setVisible(true);
+        });
+    }
+    
+}
